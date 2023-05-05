cf8e8d103d706bdf21e0c047cd5b7559777d8556
vova <sevlabr@yandex.ru>
2023-05-05 20:03:02 +0300

    Testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,32 +7,19 @@
 use core::panic::PanicInfo;
 use rs_os::println;
 
-#[allow(clippy::empty_loop)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rs_os::test_panic_handler(info)
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
